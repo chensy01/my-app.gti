4cc65a3d09354b197a2d9d4d7629786d2bd3d010
chensy01 <csy.mailbox@qq.com>
2016-11-06 00:25:10 +0800

    spring support

--- helloapp/src/mypack/DispatcherServlet.java
+++ helloapp/src/mypack/DispatcherServlet.java
@@ -12,6 +12,8 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import mypack.Entity.*;
+import org.springframework.context.support.*;
+import org.springframework.context.*;
 
 
 
@@ -23,19 +25,23 @@ public class DispatcherServlet extends HttpServlet{
 			super.init(config);
 
 			
-			// A SessionFactory is set up once for an application!
-			final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-					.configure() // configures settings from hibernate.cfg.xml
-					.build();
-			try {
-				sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
-			}
-			catch (Exception e) {
-				// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-				// so destroy it manually.
-				System.out.println(e.getMessage());
-				StandardServiceRegistryBuilder.destroy( registry );
-			}
+			// // A SessionFactory is set up once for an application!
+			// final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+			// 		.configure() // configures settings from hibernate.cfg.xml
+			// 		.build();
+			// try {
+			// 	sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+			// }
+			// catch (Exception e) {
+			// 	// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+			// 	// so destroy it manually.
+			// 	System.out.println(e.getMessage());
+			// 	StandardServiceRegistryBuilder.destroy( registry );
+			// }
+
+			//use spring
+			ApplicationContext ac = new FileSystemXmlApplicationContext("applicationContext.xml");
+			sessionFactory = (SessionFactory)ac.getBean("sessionFactory");
 		
 	}
 
